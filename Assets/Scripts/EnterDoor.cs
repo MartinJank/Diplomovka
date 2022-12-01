972c3fb196fa58bf04104782c54e551e5bbfc681
Martin Jankuliak <43377338+MartinJank@users.noreply.github.com>
2022-12-01 15:36:34 +0100

    minor changes

--- Assets/Scripts/EnterDoor.cs
+++ Assets/Scripts/EnterDoor.cs
@@ -12,31 +12,36 @@ public class EnterDoor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter2D(Collider2D otherObject) {
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
         if (otherObject.gameObject.tag == playerTag)
         {
 
-            if (scene == "Minigame" || scene == "Work") {
-                if (!GameControl.control.isExhausted) {
+            if (scene == "Minigame" || scene == "Work")
+            {
+                if (!GameControl.control.isExhausted)
+                {
                     GameControl.control.isExhausted = true;
                     SceneManager.LoadScene(scene);
                 }
-            } else {
+            }
+            else
+            {
                 SceneManager.LoadScene(scene);
             }
 
             // SceneManager.LoadScene(scene);
             // Debug.Log("collision");
-            
+
             // displayExp.text = "Experience: " + GameControl.control.exp;
         }
     }
