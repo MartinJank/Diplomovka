51a816c4265c753eabbf254be64c30a5afa78d23
Martin Jankuliak <43377338+MartinJank@users.noreply.github.com>
2022-12-06 23:04:09 +0100

    prompt text above head

--- Assets/Scripts/EnterDoor.cs
+++ Assets/Scripts/EnterDoor.cs
@@ -8,43 +8,65 @@ public class EnterDoor : MonoBehaviour
 {
     [SerializeField] private string playerTag;
     [SerializeField] private string scene;
-    // [SerializeField] private TextMeshProUGUI displayExp;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    [SerializeField] private bool triggerActive = false;
+    [SerializeField] private GameObject uiPanel;
 
     private void OnTriggerEnter2D(Collider2D otherObject)
     {
-        if (otherObject.gameObject.tag == playerTag)
+        // if (otherObject.gameObject.tag == playerTag)
+        // {
+
+            // if (scene == "Minigame" || scene == "Work")
+            // {
+            //     if (!GameControl.control.isExhausted)
+            //     {
+            //         GameControl.control.isExhausted = true;
+            //         SceneManager.LoadScene(scene);
+            //     }
+            // }
+            // else
+            // {
+            //     SceneManager.LoadScene(scene);
+            // }
+
+        // }
+
+        if (otherObject.CompareTag(playerTag))
         {
-
-            if (scene == "Minigame" || scene == "Work")
-            {
-                if (!GameControl.control.isExhausted)
-                {
-                    GameControl.control.isExhausted = true;
-                    SceneManager.LoadScene(scene);
-                }
-            }
-            else
-            {
-                SceneManager.LoadScene(scene);
-            }
-
-            // SceneManager.LoadScene(scene);
-            // Debug.Log("collision");
-
-            // displayExp.text = "Experience: " + GameControl.control.exp;
+            triggerActive = true;
+            uiPanel.SetActive(true);
         }
     }
 
+    private void OnTriggerExit2D(Collider2D otherObject) {
+        if (otherObject.CompareTag("Player"))
+        {
+            triggerActive = false;
+            uiPanel.SetActive(false);
+        }
+    }
+
+    private void Update()
+        {
+            if (triggerActive && Input.GetKeyDown(KeyCode.Space))
+            {
+                OpenDoor();
+            }
+        }
+    public void OpenDoor()
+    {
+        if (scene == "Minigame" || scene == "Work")
+        {
+            if (!GameControl.control.isExhausted)
+            {
+                GameControl.control.isExhausted = true;
+                SceneManager.LoadScene(scene);
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
 
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private SpriteRenderer sprite;
     [SerializeField] private float speed;
+    [SerializeField] private GameObject uiPanel;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        uiPanel.SetActive(false);
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
