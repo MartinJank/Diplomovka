fe3af6c1b26b591cc02efe26940efa20b2e8020d
Martin Jankuliak <43377338+MartinJank@users.noreply.github.com>
2023-01-02 15:33:59 +0100

    menu scene added

---
+++ Assets/Dynamic Flickering Light/FlickeringLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FlickeringLight : MonoBehaviour
+{
+    // public Transform flickerLight;
+    public UnityEngine.Rendering.Universal.Light2D flickerLightComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // flickerLight = this.transform.GetChild(1);
+        // flickerLightComponent = flickerLight.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            float randomIntensity = Random.Range(0.0f, 1.2f);
+            flickerLightComponent.intensity = randomIntensity;
+
+
+            float randomTime = Random.Range(0.2f, 1.0f);
+            yield return new WaitForSeconds(randomTime);
+        }
+    }
+}
