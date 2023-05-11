3efcce13d73a78c2bfd30a566f594480f69e7da8
Martin Jankuliak <m.jankuliak@gmail.com>
2023-05-11 09:13:30 +0200

    minor changes

--- Assets/Scripts/MakeEverytingZero.cs
+++ Assets/Scripts/MakeEverytingZero.cs
@@ -10,6 +10,8 @@ public class MakeEverytingZero : MonoBehaviour
         GameControl.control.day = 1;
         GameControl.control.money = 100;
         GameControl.control.exp = 5;
+        // GameControl.control.level = 5;
+        GameControl.control.exp = 5;
         GameControl.control.loadedScenes.Clear();
     }
 }
